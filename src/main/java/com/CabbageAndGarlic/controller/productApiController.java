4b1568945886eadd62d14ffdf8bc0756a975a344
bumeruchi <jack1871@naver.com>
2024-06-17 15:55:56 +0900

    PM:3:55

---
+++ src/main/java/com/CabbageAndGarlic/controller/productApiController.java
@@ -0,0 +1,14 @@
+package com.CabbageAndGarlic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class productApiController {
+
+
+}
--- src/main/java/com/CabbageAndGarlic/controller/productViewController.java
+++ src/main/java/com/CabbageAndGarlic/controller/productViewController.java
@@ -1,17 +1,16 @@
 package com.CabbageAndGarlic.controller;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class testController {
-    @GetMapping(value = "/test")
-    public String reply(Model model) {
+public class productViewController {
 
-        return "mesView";
+    @GetMapping(value = "/test")
+    public String product(Model model) {
+
+        return "productView";
     }
 
 }
