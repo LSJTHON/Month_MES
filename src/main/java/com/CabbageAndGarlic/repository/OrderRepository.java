02547e41cd867b7a112027bea1a69d6ab112e6ad
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-06-18 12:21:07 +0900

    파일 삭제
    2024-06-18

--- src/main/java/com/CabbageAndGarlic/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CabbageAndGarlic.repository;
-
-import com.CabbageAndGarlic.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
