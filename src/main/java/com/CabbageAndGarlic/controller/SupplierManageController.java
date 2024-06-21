c7d1746c29f6b63d406ca395d1db4bb357697bda
dmswl620 <154316295+dmswl620@users.noreply.github.com>
2024-06-21 16:57:30 +0900

    html적용중

--- src/main/java/com/CabbageAndGarlic/controller/SupplierManageController.java
+++ src/main/java/com/CabbageAndGarlic/controller/SupplierManageController.java
@@ -1,14 +1,9 @@
 package com.CabbageAndGarlic.controller;
 
-import com.CabbageAndGarlic.dto.SupplierManageDto;
-import com.CabbageAndGarlic.service.SupplierManageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/supplier")
@@ -16,7 +11,7 @@ public class SupplierManageController {
 
     @GetMapping
     public String getSupplierManagementPage(Model model) {
-        return "/Supplier/Supplier";
+        return "/Supplier/supplier";
     }
 }
 
